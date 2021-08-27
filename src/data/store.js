f21bac3f806161e1b7cbd59b61d3200fe8fa6bea
Kyrylo Ovsiannik <ovsyasha16@gmail.com>
2021-08-27 16:36:35 +0300

    80% front done without api

--- src/data/store.js
+++ src/data/store.js
@@ -6,6 +6,23 @@ const store = {
         img: 'https://www.dallalii.com/img/admin/avatar5.png', location: 'London', favoriteTags: 'PHP, CSS'},
         {id: 3, key:'qwerty3', name: 'Arnold', reputation: 'low', date: '23.12.2010', votes: 'votes3', answer: 'answer3', views: 'views3',
         img: 'https://www.dallalii.com/img/admin/avatar5.png', location: 'Paris', favoriteTags: 'C, C++'},
+    ],
+    tagsInfo: [
+        {id: 55, tagName: 'javascript', totalNumberQuestion: '3423432', askedToday: '807', 
+        text: 'For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript).'},
+        {id: 56, tagName: 'java', totalNumberQuestion: '3423432', askedToday: '333807',
+        text: 'For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript).'},
+        {id: 57, tagName: 'python', totalNumberQuestion: '3412312', askedToday: '4443807',
+        text: 'For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript).'},
+        {id: 58, tagName: 'C#', totalNumberQuestion: '34236877972', askedToday: '1807',
+        text: 'For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript).'},
+        {id: 59, tagName: 'C++', totalNumberQuestion: '32', askedToday: '807',
+        text: 'For questions regarding programming in ECMAScript (JavaScript/JS) and its various dialects/implementations (excluding ActionScript).'},
+    ],
+    profileInfo: [
+        {id: 44, reputation: 23, posts: 234, questions: 54, answers: 22, coments: 3, favorites: 5,  notifications: 77, privelegies: 'privelegies', tags: 3, bages: 'STAR'},
+        {id: 45, reputation: 22, posts: 224, questions: 547, answers: 224, coments: 31, favorites: 50,  notifications: 727, privelegies: 'privelegies2', tags: 3, bages: 'STAR'},
+        {id: 46, reputation: 277, posts: 3454, questions: 542, answers: 223333, coments: 30, favorites: 540,  notifications: 7237, privelegies: 'privelegies3', tags: 3, bages: 'STAR'}
     ]
 }
 
